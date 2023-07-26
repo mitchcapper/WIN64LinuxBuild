e5e10085382a95a78a837f301bd7bf66a0418ef4
Mitch Capper <mitch.capper@gmail.com>
2023-07-26 01:53:03 -0700

    Disable msys2 arg conversion completely

--- msys_shell.ps1
+++ msys_shell.ps1
@@ -3,8 +3,8 @@ Set-StrictMode -version latest;
 $ErrorActionPreference = "Stop";
 
 $env:MSYS="winsymlinks:native wincmdln"
-$env:MSYS2_ARG_CONV_EXCL="/a;/b;/c;/d;/e;/f;/g;/h;/i;/j;/k;/l;/m;/n;/o;/p;/q;/r;/s;/u;/v;/w;/x;/y;/z;/0;/1;/2;/3;/4;/5;/6;/7;/8;/9;/A;/B;/C;/D;/E;/F;/G;/H;/I;/J;/K;/L;/M;/N;/O;/P;/Q;/R;/S;/T;/U;/V;/W;/X;/Y;/Z"
-
+#$env:MSYS2_ARG_CONV_EXCL="/a;/b;/c;/d;/e;/f;/g;/h;/i;/j;/k;/l;/m;/n;/o;/p;/q;/r;/s;/u;/v;/w;/x;/y;/z;/0;/1;/2;/3;/4;/5;/6;/7;/8;/9;/A;/B;/C;/D;/E;/F;/G;/H;/I;/J;/K;/L;/M;/N;/O;/P;/Q;/R;/S;/T;/U;/V;/W;/X;/Y;/Z"
+$env:MSYS2_ARG_CONV_EXCL="*"
 if ($env:WLB_BASE_FOLDER){
 	cd $env:WLB_BASE_FOLDER;
 }
