3be785e12e3a209ee068d771bd02f9545ca21bdf
Mitch Capper <mitch.capper@gmail.com>
2024-12-16 03:58:03 -0800

    gawk and automake updates

--- build/f_automake_build.sh
+++ build/f_automake_build.sh
@@ -4,42 +4,39 @@ set -e
 
 BLD_CONFIG_BUILD_NAME="automake"
 BLD_CONFIG_GNU_LIBS_USED=0
+BLD_CONFIG_BUILD_DEBUG=0 #never use debug we dont have an advantage and it leaves artifacts it then compalins about
+
 BLD_CONFIG_BUILD_MSVC_RUNTIME_INFO_ADD_TO_C_AND_LDFLAGS=1
 
 # BLD_CONFIG_BUILD_FOLDER_NAME="myapp2"; #if you want it compiling in a diff folder
 # BLD_CONFIG_BUILD_DEBUG=1
 
 function ourmain() {
-	#export CFLAGS="-DDEC -I/oth $CFLAGS"
-	#export LDFLAGS=" $LDFLAGS" #for additional libs use CONFIG_ADDL_LIBS
 	startcommon;
 
 if test 5 -gt 100; then
 		echo "Just move the fi down as you want to skip steps, or pass the step to skip to (per below) as the first arg"
 fi
 	if [[ -z $SKIP_STEP || $SKIP_STEP == "checkout" ]]; then
-		git clone --recurse-submodules https://github.com/autotools-mirror/automake .
-		add_items_to_gitignore;
-		SKIP_STEP=""
+		git_clone_and_add_ignore https://github.com/autotools-mirror/automake .
 	fi
 
-	if [[ -z $SKIP_STEP || $SKIP_STEP == "our_patch" ]]; then
-
-		apply_our_repo_patch; #looks in the patches folder for  repo_BUILD_NAME.patch and applies it.  Easy way to generate the patch from modified repo, go to your modified branch (make sure code committed) and run: git diff --color=never master > repo_NAME.patch
-
+	PERL_P=$(convert_to_msys_path "${BLD_CONFIG_SRC_FOLDER}/automake-1.17")
+	export PERL5LIB="${PERL_P}"
+	if [[ -z $SKIP_STEP || $SKIP_STEP == "bootstrap_fix" ]]; then
+		attrib -r /s "bin/*"
+		attrib -r /s "t/*"
+		mv bootstrap bootstrap.in
+		head bootstrap.in -n -3 > bootstrap #remove lines that remove the temp dir we need for the perl module
+		ex ./bootstrap
 		SKIP_STEP=""
 	fi
-
 	cd $BLD_CONFIG_SRC_FOLDER
 	if [[ -z $SKIP_STEP || $SKIP_STEP == "configure" ]]; then
-
 		configure_apply_fixes_and_run;
-
-		SKIP_STEP="";
 	else
 		setup_build_env;
 	fi
-	cd $BLD_CONFIG_SRC_FOLDER
 
 	run_make
 	make_install
--- build/f_gawk_build.sh
+++ build/f_gawk_build.sh
@@ -2,29 +2,25 @@
 set -e
 . "${WLB_SCRIPT_FOLDER:-$(dirname "$(readlink -f "$BASH_SOURCE")")}/helpers.sh"
 
-BLD_CONFIG_BUILD_NAME="gawk";
-
-#BLD_CONFIG_BUILD_DEBUG=1
-BUILD_MSVC_IGNORE_WARNINGS=(4068)
+BLD_CONFIG_BUILD_NAME="gawk"
+BLD_CONFIG_GNU_LIBS_ADD_TO_REPO=1
 BLD_CONFIG_BUILD_MSVC_RUNTIME_INFO_ADD_TO_C_AND_LDFLAGS=1
 BLD_CONFIG_GNU_LIBS_ADDL=( "mkstemp" "fts" "sys_socket" "strcasestr" "regex" "random" "flexmember" "setlocale" "locale" "dfa" "sleep" "strsignal" "sys_ioctl" "connect" "listen" "accept" "fnmatch-h" "fnmatch-gnu" "recvfrom" "bind" "setsockopt" "getsockopt" "getopt-gnu" "shutdown" "sys_random" "popen" "pclose" "socket" "strcase" "timegm" "setenv" "unsetenv" "usleep" "fprintf-gnu" )
+BLD_CONFIG_BUILD_MSVC_IGNORE_WARNINGS=( "4068" )
+BLD_CONFIG_CONFIG_CMD_ADDL=( "ac_cv_search_dlopen=\"none required\"" "--enable-extensions" "--enable-threads=windows" "acl_shlibext=dll" "ac_cv_header_dlfcn_h=yes" )
+BLD_CONFIG_BUILD_MAKE_CMD_ADDL=( "DEFPATH=\"\\\"./;%%PROGRAMDATA%%/gawk/share\\\"\"" "DEFLIBPATH=\"\\\"./;%%PROGRAMDATA%%/gawk/lib\\\"\"" )
 
-BLD_CONFIG_CONFIG_CMD_ADDL=(ac_cv_search_dlopen="none required" "--enable-extensions" "--enable-threads=windows" "acl_shlibext=dll" "ac_cv_header_dlfcn_h=yes")
-BLD_CONFIG_PREFER_STATIC_LINKING=0 #we need shared for extension dlls not sure if we can build both at once
-BLD_CONFIG_ADD_WIN_ARGV_LIB=0
+# BLD_CONFIG_BUILD_FOLDER_NAME="myapp2"; #if you want it compiling in a diff folder
+# BLD_CONFIG_BUILD_DEBUG=1
+
 function ourmain() {
 	startcommon;
-	 if [[ $BLD_CONFIG_PREFER_STATIC_LINKING -eq 1 ]]; then
-	 	BLD_CONFIG_CONFIG_CMD_ADDL+=("--enable-static" "--disable-shared")
-	 else
-	 	BLD_CONFIG_CONFIG_CMD_ADDL+=("--disable-static" "--enable-shared")
-	 fi
+
 if test 5 -gt 100; then
 		echo "Just move the fi down as you want to skip steps, or pass the step to skip to (per below) as the first arg"
 fi
 	if [[ -z $SKIP_STEP || $SKIP_STEP == "checkout" ]]; then
-		git_clone https://git.savannah.gnu.org/git/gawk.git .
-		git submodule add https://github.com/coreutils/gnulib
+		git_clone_and_add_ignore https://git.savannah.gnu.org/git/gawk.git .
 		cp gnulib/build-aux/bootstrap .
 		cp gnulib/build-aux/bootstrap.conf .
 		echo "gnulib_tool_option_extras=\" --without-tests --symlink --m4-base=m4 --lib=libgawk --source-base=lib --cache-modules\"" >> bootstrap.conf
@@ -39,59 +35,49 @@ fi
 		echo "EXTRA_DIST = " > m4/Makefile.am
 		add_items_to_gitignore;
 		git_staging_add bootstrap bootstrap.conf
-		git_staging_commit #need to commit it up so that the bootstrap files are avail for our gnulib patching by default all local changes are stashed		
-		SKIP_STEP=""
+		git_staging_commit #need to commit it up so that the bootstrap files are avail for our gnulib patching by default all local changes are stashed	
+	fi
+	if [[ -z $SKIP_STEP || $SKIP_STEP == "our_patch" ]]; then
+		apply_our_repo_patch; # Applies from patches folder repo_BUILD_NAME.patch to the sources
 	fi
 	if [[ $BLD_CONFIG_BUILD_DEBUG -eq 1 ]]; then
 		touch .developing
 	else
 		rm .developing &>/dev/null || true
 	fi
-	if [[ -z $SKIP_STEP || $SKIP_STEP == "our_patch" ]]; then
-		apply_our_repo_patch; #looks in the patches folder for  repo_BUILD_NAME.patch and if found applies it.  Easy way to generate the patch from modified repo, go to your modified branch (make sure code committed) and run: git diff --color=never master > repo_NAME.patch
-		SKIP_STEP=""
-	fi
 	if [[ $BLD_CONFIG_GNU_LIBS_USED -eq "1" ]]; then
 		if [[ -z $SKIP_STEP || $SKIP_STEP == "gnulib" ]]; then
 			gnulib_switch_to_master_and_patch;
-			SKIP_STEP=""
 		fi
 		cd $BLD_CONFIG_SRC_FOLDER
-
 		if [[ -z $SKIP_STEP || $SKIP_STEP == "bootstrap" ]]; then
-			gnulib_add_addl_modules_to_bootstrap;		
-			gnulib_ensure_buildaux_scripts_copied;
-			libtoolize --install --verbose #this is normally part of autoreconf to install the build-aux we removed earlier like ltmain.sh as we need the right version  while bootstrap will run autoreconf it wont do the install at the parent dir level.  We also want this run before the bootstrap so as to not override gnulibs installs
-			setup_gnulibtool_py_autoconfwrapper #needed for generated .mk/.ac files but if just stock then the below line likely works
-			libtool_fixes "build-aux/ltmain.sh" "m4/libtool.m4"
-			./bootstrap --no-bootstrap-sync --no-git --gnulib-srcdir=gnulib --skip-po --force
-			gnulib_ensure_buildaux_scripts_copied --forced;
-			SKIP_STEP=""
+			gnulib_add_addl_modules_and_bootstrap;
 		fi
+		autoreconf --symlink --verbose --install --force #needed to get libtool
+		gnulib_ensure_buildaux_scripts_copied --force
+		libtool_fixes
+	fi
+
+	if [[ $SKIP_STEP == "autoconf" ]]; then #not empty allowed as if we bootstrapped above we dont need to run nautoconf
+		autoreconf --symlink --verbose --install
+		SKIP_STEP=""
 	fi
 
 	cd $BLD_CONFIG_SRC_FOLDER
 	if [[ -z $SKIP_STEP || $SKIP_STEP == "configure" ]]; then
-		configure_fixes;
-		configure_run;
+		configure_apply_fixes_and_run;
 		echo "#include <osfixes.h>" > "lib/dlfcn.h"
-		SKIP_STEP="";
 	else
 		setup_build_env;
 	fi
 
-	MAKE_ADD=('DEFPATH="\"./;%%PROGRAMDATA%%/gawk/share\""' 'DEFLIBPATH="\"./;%%PROGRAMDATA%%/gawk/lib\""')
-	if [[ -n "${LOG_MAKE_RUN}" ]]; then
-		run_logged_make $BLD_CONFIG_BUILD_MAKE_BIN "${MAKE_ADD[@]}";
-	fi
-	ex $BLD_CONFIG_BUILD_MAKE_BIN "${MAKE_ADD[@]}" -j 8 ||  ex $BLD_CONFIG_BUILD_MAKE_BIN "${MAKE_ADD[@]}"
-	make_install "${MAKE_ADD[@]}"
+	run_make
+	make_install
 	mkdir -p $BLD_CONFIG_INSTALL_FOLDER/ProgramData/lib $BLD_CONFIG_INSTALL_FOLDER/ProgramData/share $BLD_CONFIG_INSTALL_FOLDER/ProgramData/libexec
 	mv $BLD_CONFIG_INSTALL_FOLDER/lib/gawk/* $BLD_CONFIG_INSTALL_FOLDER/ProgramData/lib/
 	mv $BLD_CONFIG_INSTALL_FOLDER/libexec/awk/* $BLD_CONFIG_INSTALL_FOLDER/ProgramData/libexec/
 	mv $BLD_CONFIG_INSTALL_FOLDER/share/awk/* $BLD_CONFIG_INSTALL_FOLDER/ProgramData/share/
 	rmdir $BLD_CONFIG_INSTALL_FOLDER/lib/gawk/ $BLD_CONFIG_INSTALL_FOLDER/libexec/awk/ $BLD_CONFIG_INSTALL_FOLDER/share/awk/ $BLD_CONFIG_INSTALL_FOLDER/lib/ $BLD_CONFIG_INSTALL_FOLDER/libexec/
-
 	finalcommon;
 }
 ourmain;
