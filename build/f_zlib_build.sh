4959c5d7c0b3e92bb03d5033fbced0eaa87b8e6d
Mitch Capper <mitch.capper@gmail.com>
2025-02-05 15:14:55 -0800

    Fix zlib build switch away from msys cmake to nmake

--- build/f_zlib_build.sh
+++ build/f_zlib_build.sh
@@ -9,6 +9,11 @@ BLD_CONFIG_GNU_LIBS_USED=0
 BLD_CONFIG_GIT_CLONE_BRANCH="develop"
 BLD_CONFIG_CMAKE_CONFIG_CMD_ADDL=( "-DINSTALL_PKGCONFIG_DIR:PATH=[INSTALL_FOLDER]/lib/pkgconfig" "-DENABLE_BINARY_COMPATIBLE_POSIX_API:BOOL=1" )
 
+
+
+# ninja, nmake, nmake-launchers, vs  all work msys(general default) does not
+BLD_CONFIG_CMAKE_STYLE="nmake"
+
 # BLD_CONFIG_BUILD_FOLDER_NAME="myapp2"; #if you want it compiling in a diff folder
 # BLD_CONFIG_BUILD_DEBUG=1
 
@@ -21,14 +26,15 @@ fi
 	if [[ -z $SKIP_STEP || $SKIP_STEP == "checkout" ]]; then
 		git_clone_and_add_ignore https://github.com/madler/zlib.git .
 	fi
-
+	
+	cd $BLD_CONFIG_SRC_FOLDER
 	if [[ -z $SKIP_STEP || $SKIP_STEP == "our_patch" ]]; then
 		apply_our_repo_patch; # Applies from patches folder repo_BUILD_NAME.patch to the sources
 		if [[ $BLD_CONFIG_BUILD_DEBUG -eq 1 ]]; then
 			sed -i -E 's/-lz/-lzd/' zlib.pc.cmakein
 		fi
 		if [[ $BLD_CONFIG_PREFER_STATIC_LINKING -eq 1 ]]; then
-			sed -i -E 's/^sharedlibdir.+//;s/-L\$\{sharedlibdir\}//;s/-lz/-lzlibstatic/' zlib.pc.cmakein
+			sed -i -E 's/^sharedlibdir.+//;s/-L\$\{sharedlibdir\}//;s/-lz/-lzs/' zlib.pc.cmakein
 		fi
 		sed -i -E 's/if\(MSVC\)/if(MSVC OR NOT MSVC)/' CMakeLists.txt
 		git_staging_add zlib.pc.cmakein .gitignore CMakeLists.txt #staging them means if we re-apply our patch they are discarded
